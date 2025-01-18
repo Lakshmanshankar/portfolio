2a8e473c7069d8d66c9831a3272957d0d8b8ed44
Lakshmanshankar <lakshmanshankarc@gmail.com>
2025-01-18 13:19:24 +0530

    test-bullet-subdirectory

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  rewrites() {
+    const bulletUrl = new URL(`https://mariod-bullet.pages.dev/`).origin;
+    return [
+      {
+        source: "/blog",
+        destination: `${bulletUrl}`,
+      }, // match all paths with extension eg .xml, .json etc.
+      {
+        source: "/blog/:slug(.*\\.[a-zA-Z]{2,5}$)",
+        destination: `${bulletUrl}/:slug`,
+      }, // match all paths starting with /blog
+      {
+        source: "/blog/:path*",
+        destination: `${bulletUrl}/:path*/`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
